6ce50a409fa4c782b15871840ccb7b9e0bacc5fd
Torsten Curdt <tcurdt@vafer.org>
2018-07-03 00:38:08 +0200

    use keys defined in web ui

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -3,19 +3,17 @@
 : "${EMAIL:?EMAIL is mising}"
 : "${SSH_PRIVATE_KEY:?SSH_PRIVATE_KEY is mising}"
 
-exit 0
-
 git config --global user.name "Travis CI"
 git config --global user.email "$EMAIL"
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-# echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 
 eval "$(ssh-agent -s)"
 
-echo "$SSH_PRIVATE_KEY" | base64 --decode | ssh-add - > /dev/null
-# echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+# echo "$SSH_PRIVATE_KEY" | base64 --decode | ssh-add - > /dev/null
 # echo "$SSH_PRIVATE_KEY" | base64 --decode > .ssh/travis
 # chmod 600 .ssh/travis
 # ssh-add .ssh/travis
