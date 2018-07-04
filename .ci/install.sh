d060f228f1f4bc5d90a12f4a74c96f1de4d6b8e5
Torsten Curdt <tcurdt@vafer.org>
2018-07-04 17:39:45 +0200

    debugging

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,4 +4,7 @@ set -e
 mkdir -p bin
 curl -sL `curl -s https://api.github.com/repos/tcnksm/ghr/releases/latest | jq -r '.assets[] | select(.name | test("linux_amd64.tar.gz"; "ix")) | .browser_download_url'` | tar -xz --strip-components=1 -C bin
 
+pwd
+ls -la bin
+echo $PATH
 
