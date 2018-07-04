bd36d98d6789c340f2231e89aaf5b10b376893a4
Torsten Curdt <tcurdt@vafer.org>
2018-07-04 18:18:42 +0200

    less magic

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,4 +3,7 @@ set -e
 
 mkdir -p bin
 
-curl -sL `curl -s https://api.github.com/repos/tcnksm/ghr/releases/latest | jq -r '.assets[] | select(.name | test("linux_amd64.tar.gz"; "ix")) | .browser_download_url'` | tar -xz --strip-components=1 -C bin
+# curl -s https://api.github.com/repos/tcnksm/ghr/releases/latest | jq -r '.assets[] | select(.name | test("linux_amd64.tar.gz"; "ix")) | .browser_download_url'
+URL_GHR=https://github.com/tcnksm/ghr/releases/download/v0.10.0/ghr_v0.10.0_linux_amd64.tar.gz
+
+curl -sL $URL_GHR | tar -xz --strip-components=1 -C bin
