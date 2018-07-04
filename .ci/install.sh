8acbba5b21525e036d69ed030028e347ddd79df8
Torsten Curdt <tcurdt@vafer.org>
2018-07-04 18:02:12 +0200

    now

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,8 +2,5 @@
 set -e
 
 mkdir -p bin
-curl -sL `curl -s https://api.github.com/repos/tcnksm/ghr/releases/latest | jq -r '.assets[] | select(.name | test("linux_amd64.tar.gz"; "ix")) | .browser_download_url'` | tar -xz --strip-components=1 -C bin
 
-export PATH=$PATH:`pwd`/bin
-ls -la bin/ghr
-type ghr
+curl -sL `curl -s https://api.github.com/repos/tcnksm/ghr/releases/latest | jq -r '.assets[] | select(.name | test("linux_amd64.tar.gz"; "ix")) | .browser_download_url'` | tar -xz --strip-components=1 -C bin
